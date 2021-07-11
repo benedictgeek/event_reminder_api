5302df9bd30bdf0d37f155b0f186b5037b6f064e
olushola <olushola.dauda@algorismng.com>
2021-07-11 14:54:31 +0100

    setting up project structure

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const http = require("http");
 const express = require("express");
-
 const app = express();
 
-app.post("/", (req, res, _) => {});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/event", (req, res, _) => {});
+
+http.createServer(app).listen(3040);
